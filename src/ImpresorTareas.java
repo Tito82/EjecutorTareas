b45e84baf3179f719f0caf49a18df04201d4507f
Fernando Ares <fernandoareslouro@gmail.com>
2021-10-01 12:28:42 +0200

    arreglo de ramdom

--- src/ImpresorTareas.java
+++ src/ImpresorTareas.java
@@ -3,29 +3,29 @@ import java.security.SecureRandom;
 public class ImpresorTareas implements Runnable {
 	
 	private static final SecureRandom generador = new SecureRandom();
-	private static int sleepTime;
+	private final int sleepTime;
 	private final String nombreTarea;
-	
+
 	
 	public ImpresorTareas(String nombreTarea) {
 		this.nombreTarea = nombreTarea;
 		
 		sleepTime = generador.nextInt(5000);
+		//El 5000 es el límite de tiempo que precisa el sleeptime
 	}
 	
-
 	@Override
 	public void run() {
-		
 		try {
-			System.out.printf("%s se fue a dormir %d milisegundos %n%n", this.nombreTarea, sleepTime);
+			System.out.printf("%s se fue a dormir %d milisegundos%n%n", this.nombreTarea, sleepTime);
 			Thread.sleep(sleepTime);
-		}catch (InterruptedException exception) {
-			exception.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 			Thread.currentThread().interrupt();
 		}
-		// TODO Auto-generated method stub
 		
+
 	}
 
 }
